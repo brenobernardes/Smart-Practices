5acd1a79704fcc29fa0fdf25f73043d54e9436c6
Breno Bernardes <breno.pbernardes@gmail.com>
2022-03-08 20:36:19 -0300

    adicionado textos

---
+++ src/content/content.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './content.css'
+
+function Content () {
+    return  (
+        <div className="mainContent">
+            <h2 className="sectionTitle">Segmentos</h2>
+            <li>
+                <h2>InfoProdutor e Afiliado</h2>
+                <p>Mantenha seu negócio digital legalizado e evite problemas com o fisco. Atendemos todas as plataformas digitais do mercado</p>
+            </li>
+            <li>
+                <h2>Contabilidade para E-commerce</h2>
+                <p>Se você tem uma loja de e-commerce ou atua com dropshipping mantenha suas vendas na internet legalizadas com nosso serviço de contabilidade</p>
+            </li>
+            <li>
+                <h2>Comércio de Rua</h2>
+                <p>Se você tem sua empresa, seja ela um restaurante, loja de roupas, mercados, depósito, loja de bebidas, padarias, enfim, seja qual for seu comércio, podemos lhe auxiliar com nossos serviços Contábeis e Financeiros. </p>
+            </li>
+            <li>
+                <h2>Digital Influencer</h2>
+                <p>Se você é um Digital Influencer, YouTuber e recebe valores pelos seus serviços, lhe ajudamos a legitimar essas operação e pagar o imposto devido da melhor forma.</p>
+            </li>
+            <li>
+                <h2>Prestadores de Serviços</h2>
+                <p>Se você é um Prestador de Serviços, regularize sua empresa conosco e tenha a opção de emitir notas fiscais dos seus serviços prestados</p>
+            </li>
+            <li>
+                <h2>Contabilidade para Serviços Online</h2>
+                <p>Você que oferece serviços online através de plataformas digitais tais como a Hotmart ou Eduzz você precisa ter o seu CNPJ para receber seus rendimentos de forma legal.</p>
+            </li>
+        </div>
+    );
+}
+
+export default Content;
