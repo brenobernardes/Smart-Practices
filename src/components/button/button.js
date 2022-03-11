d33573e617111ec55424d1cf720f5218835e4fc9
Breno Bernardes <breno.pbernardes@gmail.com>
2022-03-10 22:24:50 -0300

    adicionado botao

---
+++ src/components/button/button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../button/button.css";
+
+function Button () {
+    return (
+        <button className="button">Saiba mais</button>
+    );
+}
+
+export default Button;
