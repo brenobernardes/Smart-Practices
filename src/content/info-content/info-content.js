650fe2d901531e43d1d657d7138f0e7616100e0c
Breno Bernardes <breno.pbernardes@gmail.com>
2022-03-13 22:48:36 -0300

    adicionado informaçao de contato

---
+++ src/content/info-content/info-content.js
@@ -0,0 +1,26 @@
+import React from "react";
+import '../info-content/info-content.css';
+
+function InfoContact () {
+
+    return (
+        <div className="contactInfo">
+            <div>
+            
+            </div>
+            <div>
+                <h2>Horário de Funcionamento</h2>
+                <p>Segunda-feira - Sexta-feira 09:00 am - 12:00 pm / 01:00 pm - 06:00 pm</p>
+                <p>Sábado - Domingo Fechado</p>
+                <h3>Endereço</h3>
+                <p>João Gomes, 19 Belo Horizonte - MG - Brasil</p>
+                <h3>Telefone</h3>
+                <p>+55 31 99274-4214</p>
+                <h3>E-mail</h3>
+                <p>contato@smartpractices.com.br</p>
+            </div>
+        </div>
+    );
+}
+
+export default InfoContact;
